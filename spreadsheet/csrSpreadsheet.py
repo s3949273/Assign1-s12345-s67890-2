b855dfa5636850ad614939ed17261994f5c2abf5
AnDrOiDTiTaN1263 <54924407+AnDrOiDTiTaN1263@users.noreply.github.com>
2023-04-15 21:50:12 +1000

    update to insertCol function

--- spreadsheet/csrSpreadsheet.py
+++ spreadsheet/csrSpreadsheet.py
@@ -1,5 +1,6 @@
 from spreadsheet.baseSpreadsheet import BaseSpreadsheet
 from spreadsheet.cell import Cell
+from test import create_random_cells
 
 # ------------------------------------------------------------------------
 # This class is required TO BE IMPLEMENTED
@@ -8,9 +9,6 @@ from spreadsheet.cell import Cell
 # __author__ = 'Jeffrey Chan'
 # __copyright__ = 'Copyright 2023, RMIT University'
 # ------------------------------------------------------------------------
-
-
-
 
 class CSRSpreadsheet(BaseSpreadsheet):
 
@@ -75,8 +73,9 @@ class CSRSpreadsheet(BaseSpreadsheet):
         print("col:",self.col_array, self.col_array.__len__())
         print("val:",self.val_array, self.val_array.__len__())
         print("sum:",self.sum_array, self.sum_array.__len__())
+        print()
 
-    def buildSpreadsheet(self, lCells: list[Cell]):
+    def buildSpreadsheet(self, lCells: [Cell]):
         """
         Construct the data structure to store nodes.
         @param lCells: list of cells to be stored
@@ -104,17 +103,27 @@ class CSRSpreadsheet(BaseSpreadsheet):
                 cur_sum =0
                 cur_row = lCells[x].row
             cur_sum +=lCells[x].val
-
-        
+     
+    def build_sum_array(self ):
+        prev = self.col_array[0]
+        sum_arr = []
+        for x in self.col_array:
+            if x < prev:
+                sum_arr.append(x)
+        pass
+    
     def appendRow(self):
         """
         Appends an empty row to the spreadsheet.
 
         @return True if operation was successful, or False if not.
         """
-
-        # TO BE IMPLEMENTED
-        pass
+        try:
+            self.sum_array.append(self.sum_array[-1])
+            return True
+        except:
+            #something went wrong
+            return False
 
     def appendCol(self):
         """
@@ -122,10 +131,10 @@ class CSRSpreadsheet(BaseSpreadsheet):
 
         @return True if operation was successful, or False if not.
         """
-
-        # TO BE IMPLEMENTED
-        pass
-
+        try:
+            return True
+        except:
+            return False
 
     def insertRow(self, rowIndex: int)->bool:
         """
@@ -139,20 +148,42 @@ class CSRSpreadsheet(BaseSpreadsheet):
         # REPLACE WITH APPROPRIATE RETURN VALUE
         return True
 
-
     def insertCol(self, colIndex: int)->bool:
         """
         Inserts an empty column into the spreadsheet.
 
         @param colIndex Index of the existing column that will be after the newly inserted row.  If inserting as first column, specify colIndex to be 0.  If inserting a column after the last one, specify colIndex to be colNum()-1.
 
-        return True if operation was successful, or False if not, e.g., colIndex is invalid.
+        return True if operation was successful, or okFalse if not, e.g., colIndex is invalid.
         """
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return True
-
-
+        if colIndex < self.colNum():
+            if colIndex < -1:
+                return False
+            if colIndex != -1:
+                if colIndex == 0:
+                    #basically add 1 to each of the values in col_array as long as they aren't 0
+                    try:
+                        for x in self.col_array:
+                            if x != 0:
+                                x +=1
+                        return True
+                    except:
+                        return False
+                else:
+                    #colIndex > 0
+                    try:
+                        #we can't simply do range(collindex, col_array.len()) because the columns are not in order because they differ in row
+                        for x in self.col_array:
+                            if x > colIndex:
+                                x+=1
+                        return True
+                    except:
+                        return False
+            else:
+                #since col index is the last index, it basically works the same as append col, append col has no visible changes to any of the arrays
+                return True
+        else:
+            return False
 
     def update(self, rowIndex: int, colIndex: int, value: float) -> bool:
         """
@@ -170,7 +201,6 @@ class CSRSpreadsheet(BaseSpreadsheet):
         # REPLACE WITH APPROPRIATE RETURN VALUE
         return True
 
-
     def rowNum(self)->int:
         """
         @return Number of rows the spreadsheet has.
@@ -180,16 +210,12 @@ class CSRSpreadsheet(BaseSpreadsheet):
 
         return self.sum_array.__len__()-1
 
-
     def colNum(self)->int:
         """
         @return Number of column the spreadsheet has.
         """
         
         return max(self.col_array)+1
-
-
-
 
     def find(self, value: float) -> tuple[(int, int)]:
         """
@@ -205,12 +231,11 @@ class CSRSpreadsheet(BaseSpreadsheet):
         # REPLACE WITH APPROPRIATE RETURN VALUE
         return []
 
-
-
-
     def entries(self) -> list[Cell]:
         """
         return a list of cells that have values (i.e., all non None cells).
         """
 
         return []
+
+    
